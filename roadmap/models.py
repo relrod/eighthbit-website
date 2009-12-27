45e0d1369ce97eceb8d7cc4edc4d3f4c78af6427
Rick E <ricky@lightning.local>
2009-12-27 06:40:26 -0500

    Add start of roadmap app (probably broken).

---
+++ roadmap/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Project(models.Model):
+   name = models.CharField("Project Name", max_length=100)
+   description = models.TextField("Description")
+   
+   def __unicode__(self):
+      return self.name
+
+class Version(models.Model):
+   name = models.CharField("Version", max_length=15)
+   project = models.ManyToMany(Project)
+
+   def __unicode__(self):
+      return self.name
+
+class Stopsign(models.Model):
+   what = models.CharField("What is going to happen", max_length=255)
+   description = models.TextField("Extra Details")
+   project = models.ManyToMany(Project)
+   version = models.ManyToMany(Version)
+
+   def __unicode__(self):
+      return "[%s] %s" % (self.project, self.what)
+
+class Roadbump(models.Model):
+   what = models.CharField("What is the problem", max_length=255)
+   description = models.TextField("Extra Details")
+   project = models.ManyToMany(Project)
+   stopsign = models.ManyToMany(Project)
+   oldversion = models.ForeignKey(Version)
+   newversion = models.ForeignKey(Version)
+
+   def __unicode__(self):
+      return "[%s] %s - %s -> %s" % (self.project, self.what, self.oldversion, self.newversion)
+
