7348094008a7caefecbe61cea7e0135f1d8a9bda
Rick E <ricky@lightning.local>
2009-12-27 21:05:00 +0800

    Sorting in admin panel (of roadmap entries)

--- roadmap/admin.py
+++ roadmap/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from roadmap.models import *
+
+class StopsignAdmin(admin.ModelAdmin):
+   list_display = ("project","what","percent","status",)
+
 admin.site.register(Project)
 admin.site.register(Version)
-admin.site.register(Stopsign)
+admin.site.register(Stopsign, StopsignAdmin)
