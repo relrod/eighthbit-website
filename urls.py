95b8689b16c29413d4a2501666536294659920aa
Rick E <ricky@lightning.local>
2009-12-28 19:49:27 +0800

    LDAP Auth works.

--- urls.py
+++ urls.py
@@ -2,10 +2,13 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
+from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
    (r'^admin/', include(admin.site.urls)),
    (r'^$', "general.views.front"),
    (r'^wiki/(?P<page>.*)$', "wiki.views.showpage"),
    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+   (r'^login/$', login),
+   (r'^logout/$', logout),
 )
