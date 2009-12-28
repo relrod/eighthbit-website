e9f6f3a38a9c9e15003de69530f428df73b7705a
Rick E <ricky@lightning.local>
2009-12-29 05:45:41 +0800

    Wiki revisions model and page model. South migration added.

--- wiki/models.py
+++ wiki/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Page(models.Model):
-   title = models.CharField("Title", max_length=200)
-   url = models.CharField("URL", max_length=75)
+   title = models.CharField(max_length=256, unique=True)
+   revision = models.ForeignKey('Revision', related_name="page_revision")
+
+   def __unicode__(self):
+      return self.title
+
+class Revision(models.Model):
    contents = models.TextField()
+   username = models.ForeignKey(User)
+   page = models.OneToOneField(Page, related_name="page")
+   comment = models.CharField(max_length=256, blank=True)
    
    def __unicode__(self):
       return self.title
