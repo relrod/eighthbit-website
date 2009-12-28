072cdd9012c85be44f32e7fe38a91aea8d3a1478
User <codeblock@TG-1.local>
2009-12-27 20:36:43 -0500

    make wiki/views.py's showpage go by url not title. I was tired when I coded that ;)

--- wiki/views.py
+++ wiki/views.py
@@ -2,6 +2,6 @@ from eighthbit.wiki.models import Page
 from django.shortcuts import get_object_or_404, render_to_response
 
 def showpage(request, page):
-    wikipage = get_object_or_404(Page, title__iexact=page)
+    wikipage = get_object_or_404(Page, url__iexact=page)
     return render_to_response("wiki.html", {'wiki': wikipage})
 
