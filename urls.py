b654a8936686c97ff83754d9506ce04039f7f98b
Liam Dunn <liam.dunn144@gmail.com>
2009-12-30 04:50:48 +1100

    edit urls.py

--- urls.py
+++ urls.py
@@ -6,9 +6,10 @@ from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
    (r'^admin/', include(admin.site.urls)),
-   (r'^$', "general.views.front"),
-   (r'^wiki/(?P<page>.*)$', "wiki.views.showpage"),
+#   (r'^$', "general.views.front"),
+   (r'^wiki/(?P<Title>.*)$', "wiki.views.showpage"),
    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+   (r'^qdb/quote/(?P<id>.*)$', "qdb.views.showquote"),
    (r'^login/$', login),
    (r'^logout/$', logout),
 )
