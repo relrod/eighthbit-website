ad871910399e1b4c01e3628be1ec103b237d261b
User <codeblock@TG-1.local>
2009-12-29 07:42:02 -0500

    Modify wiki.Page's title field to max at 255. Not 256.

--- wiki/models.py
+++ wiki/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Page(models.Model):
-   title = models.CharField(max_length=256, unique=True)
+   title = models.CharField(max_length=255, unique=True)
    revision = models.ForeignKey('Revision', related_name="page_revision")
 
    def __unicode__(self):
