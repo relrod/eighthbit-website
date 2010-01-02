6d66a580708448b24653c095007f99c48aabb332
Rick E <ricky@lightning.local>
2010-01-02 23:52:15 +0800

    Work on bitwik views a bit. Will fit this into a template tonight, but the idea works. Will also do the html form tonight or tomorrow.

--- bitwik/views.py
+++ bitwik/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from bitwik.models import *
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
+
+def showpage(request, title):
+   # The way this works: User goes to page. If page exists, show it. If not ask if user wants to create it.
+   try:
+      page = Page.objects.get(slug=title)
+   except Page.DoesNotExist:
+#      return render_to_response("create.html",{"title": title, "404":True})
+      return HttpResponse("does not exist!")
+
+   # And get the latest revision.
+   revision = Revision.objects.filter(page=page)[0]
+
+#   return render_to_response("wiki.html",
+#         {
+#            "title": page.title,
+#            "timestamp": revision.timestamp,
+#            "local_revision": revision.local_revision,
+#            "content": revision.content,
+#            "slug": page.slug
+#         })
+   return HttpResponse("We have: " + page.title+" "+revision.content)
+
