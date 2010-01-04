d22572057cc8cc1e91a08a018fdbb5cf3d88b4fc
James S <james@nixeagle.org>
2010-01-04 04:23:21 +0000

    delete the wiki/ directory, its now in /bitwik

--- wiki/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from eighthbit.wiki.models import Page, Revision
-admin.site.register(Page)
-admin.site.register(Revision)
--- wiki/views.py
+++
@@ -1,7 +0,0 @@
-from eighthbit.wiki.models import Page
-from django.shortcuts import get_object_or_404, render_to_response
-
-def showpage(request, Title):
-    wikipage = get_object_or_404(title, url__iexact=Title)
-    return render_to_response("wiki.html", {'wiki': wikipage})
-
