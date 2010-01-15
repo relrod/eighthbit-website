f61deed89effb0be33f9363937e4884d10d9c0dc
Rick E <ricky@lightning.local>
2010-01-15 16:15:46 -0500

    Private pastes now work.

--- bitbin/views.py
+++ bitbin/views.py
@@ -2,6 +2,10 @@ from bitbin.models import *
 from django.shortcuts import get_object_or_404, render_to_response
 
 def paste(request, id):
-   paste = get_object_or_404(Bit, id=id)
+   if id[0] == "0" and id[1] == "x":
+      # Assume private paste.
+      paste = get_object_or_404(Bit, key=id)
+   else:
+      paste = get_object_or_404(Bit, id=id, key="")
    return render_to_response("bitbin/view.html", {"paste": paste})
 
