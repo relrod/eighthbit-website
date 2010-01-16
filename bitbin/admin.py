e3dbb5d8fe6ce90f52244b19f9214137a8f64caf
Rick E <ricky@lightning.local>
2010-01-16 02:41:19 -0500

    add id field to bitbin admin listing

--- bitbin/admin.py
+++ bitbin/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from bitbin.models import Bit
 
 class BitAdmin(admin.ModelAdmin):
-   list_display = ("originator","key","private")
+   list_display = ("id","originator","key","private")
 
 admin.site.register(Bit, BitAdmin)
