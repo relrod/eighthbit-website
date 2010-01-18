38ca56d510c9307deb15984f7df76580e595b721
Rick E <ricky@lightning.local>
2010-01-18 01:19:06 -0500

    Show wiki Page slug in the admin panel.

--- bitwik/admin.py
+++ bitwik/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from bitwik.models import *
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+   list_display = ("title", "slug")
+
+admin.site.register(Page, PageAdmin)
 admin.site.register(Revision)
