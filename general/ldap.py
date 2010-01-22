a08d091866e47f4b1804ec2b53a0e938feb1f066
Rick E <ricky@lightning.local>
2010-01-22 15:18:34 -0500

    make_ldap_user() and throw in some more example code.

--- general/ldap.py
+++ general/ldap.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 def ldap_login(userdict): # Expects a dictionary.
    # Go by userdict["username"] and userdict["password"]
    if login_correct:
@@ -14,4 +16,18 @@ def ldap_login(userdict): # Expects a dictionary.
          "is_staff": True
    }
 
+def make_ldap_user(userdict):
+   # Create the user object. We'll need to make sure it doesn't
+   # exist before trying to create it, but we can do that later.
+   # Do some checking from ldap_login, then:
+   user = User.objects.create_user(
+         userdict["username"],
+         userdict["email"],
+         userdict["password"]
+   )
 
+   if userdict["is_staff"] == True:
+      user.is_staff = True
+
+   user.save()
+   return user # Let's use this to return the user object.
